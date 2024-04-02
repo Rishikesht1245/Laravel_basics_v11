c2c8ae8f0a55bcce809473b3725e726b46ef1997
Rishikesh T <rishikeshtgit@gmail.com>
2024-04-02 16:37:06 +0530

    Update : completed UI for create, edit and view.

--- resources/views/note/create.blade.php
+++ resources/views/note/create.blade.php
@@ -1,3 +1,13 @@
 <x-layout>
-    <h1>Create</h1>
+    <div class="note-container single-note">
+        <h1 style="text-align: center">Create New Note</h1>
+        {{-- submitting form : it needs to go to the proper root --}}
+        <form action="{{route("note.store")}}" method="POST" class="note">
+            <textarea name="note" id="note" rows="10" class="note-body" placeholder="Enter your note here"></textarea>
+            <div class="note-buttons">
+                <a href="{{route("note.index")}}" class="note-cancel-button">Cancel</a>
+                <button class="note-submit-button">Submit</button>
+            </div>
+        </form>
+    </div>
 </x-layout>
